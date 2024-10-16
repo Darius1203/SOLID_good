8424fdeccf6691f725b6ab71a9cabcf56ef5d0d9
Deaconu Darius-Mihai <Deaconu.Do.Darius@student.utcluj.ro>
2024-10-16 13:13:51 +0300

    Added GoodO

---
+++ GoodO/src/good/Client_I.java
@@ -0,0 +1,5 @@
+package good;
+
+public interface Client_I {
+    void doSomething();
+}
---
+++ GoodO/src/good/GoodServer.java
@@ -0,0 +1,8 @@
+package good;
+
+public class GoodServer {
+
+    public void reactToClient(Client_I client) {
+        client.doSomething();
+    }
+}
